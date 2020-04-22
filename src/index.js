8a3652da08d178e9515e7c6abe30b0d31bee286e
Steffen Löbrich <steffen.loebrich@gmx.de>
2020-04-22 15:26:22 +0200

    Module complete

--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
 //jshint esversion: 6
 
-import {fetchWithTimeout} from "./services";
+import {fetchWithTimeout, fetchMovies, fetchBooks} from "./services";
 
 let movies = require("./data/movies.json");
 
-export function fetchMovies(){
-  const resolveFunction = () => movies;
-  return fetchWithTimeout(1000).then(resolveFunction);
+function getBooksAndMovies(){
+  return Promise.all([fetchBooks(), fetchMovies()])
+  .then(([books, movies]) => ({books, movies}))
+  .catch(error => console.log("Error fetching books and movies", error));
 }
 
-let moviePromise = fetchMovies();
+const getBooksAndMoviesPromise = getBooksAndMovies();
 
-moviePromise.then(function resolve(results){ console.log(results); });
+getBooksAndMoviesPromise.then(
+function(results){
+  console.log('getBooksAndMoviesPromise', results);
+});
+
+function getBooksOrMovies(){
+  return Promise.race([fetchBooks(), fetchMovies()])
+  .then(results => results)
+  .catch(error => console.log("Error waiting for the promise race", error));
+}
+
+const getBooksOrMoviesPromise = getBooksAndMovies();
+
+getBooksOrMoviesPromise.then(
+function(results){
+  console.log('getBookOrMoviesPromise', results);
+});
